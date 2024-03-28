46b371a929463aa10117e0137e39fdddbd1fd05f
Dima <D_Konon@gmail.com>
2024-03-28 22:05:27 +0300

    change 6

--- jobs/hwjob.groovy
+++ jobs/hwjob.groovy
@@ -1,7 +1,7 @@
 job('HW-20-1') {
   steps {
       folder('dir1') {
-        shell('git clone https://github.com/ussnorma/seed.git && ls -la ./seed')
+        shell('mkdir test && cd test && ./git clone https://github.com/ussnorma/seed.git && ls -la ./seed')
     }
   }
 }
