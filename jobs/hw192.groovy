f91838ebe3b3d74953b7ecdd9bf049fdeffa5813
Dima <D_Konon@gmail.com>
2024-03-28 21:06:11 +0300

    change 5

--- jobs/hw192.groovy
+++ jobs/hw192.groovy
@@ -5,15 +5,15 @@ job("hw-19-2") {
 	concurrentBuild(false)
 	steps {
 		shell("""#!/bin/python3
-import requests
+            import requests
 
-url = "https://api.nbrb.by/exrates/rates?periodicity=0"
-response = requests.get(url)
-data = response.json()
+            url = "https://api.nbrb.by/exrates/rates?periodicity=0"
+            response = requests.get(url)
+            data = response.json()
 
-for currency in data:
-    abbreviation = currency["Cur_Abbreviation"]
-    official_rate = currency["Cur_OfficialRate"]
-    print(f"{abbreviation}: {official_rate}")""")
+            for currency in data:
+                abbreviation = currency["Cur_Abbreviation"]
+                official_rate = currency["Cur_OfficialRate"]
+                print(f"{abbreviation}: {official_rate}")""")
 	}
 }
