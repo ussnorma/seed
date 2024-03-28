66185f1ac8b48c54d2beaafe8623498dc40215af
Dima <D_Konon@gmail.com>
2024-03-28 21:49:05 +0300

    change 21

--- jobs/hwjob.groovy
+++ jobs/hwjob.groovy
@@ -1,7 +1,7 @@
 job('HW-20-1') {
   steps {
-    dir("dir1") {
-      git url: 'https://github.com/ussnorma/seed', branch: 'main'
+      dir("dir1") {
+      //git url: 'https://github.com/ussnorma/seed', branch: 'main'
         sh """
           ls -la 
         """
