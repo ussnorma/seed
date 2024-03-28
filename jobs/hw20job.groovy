66cc06175bbfca7517983738179ad0b5801e6e40
Dima <D_Konon@gmail.com>
2024-03-28 21:28:04 +0300

    change 2

--- jobs/hw20job.groovy
+++ jobs/hw20job.groovy
@@ -1,11 +1,11 @@
 job('HW-20-1') {
-  steps {
-    dir('git-jobs') {
-        git url: 'https://github.com/ussnorma/seed', branch: 'main'
-        sh """
-            ls -la 
-        """
-        deleteDir()
+    steps {
+        dir('folder') {
+            git url: 'https://github.com/ussnorma/seed', branch: 'main'
+            sh """
+                ls -la 
+            """
+            deleteDir()
+        }
     }
-  }
 }
