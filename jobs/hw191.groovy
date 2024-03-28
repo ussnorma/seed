7ac12905f121aa369063574e2c8698d6ace65769
Dima <D_Konon@gmail.com>
2024-03-28 21:08:37 +0300

    change 7

--- jobs/hw191.groovy
+++ jobs/hw191.groovy
@@ -4,6 +4,7 @@ job("hw191") {
 	disabled(false)
 	concurrentBuild(false)
 	steps {
-		shell("""#!/bin/bash curl -G -d country="BY" -d year="2024" -d pretty -d key="6867f807-89d1-426a-91a6-36783f177f25" "https://holidayapi.com/v1/holidays"""")
+		shell("""#!/bin/bash 
+        curl -G -d country="BY" -d year="2024" -d pretty -d key="6867f807-89d1-426a-91a6-36783f177f25" "https://holidayapi.com/v1/holidays"""")
 	}
 }
